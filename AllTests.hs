90f5bbe6c2974da33a33f762390144b4048a4bcb
karandit <tkarandi@gmail.com>
2013-01-05 01:56:17 +0100

    module declared

--- AllTests.hs
+++ AllTests.hs
@@ -1,8 +1,10 @@
+module AllTests (main) where
+
 import Test.HUnit
 import Pr001
 import Pr008
 
-projecteuler_tests = runTestTT $ test [ 
+main = runTestTT $ test [ 
 	pr_001  ~?= 23331668
 	, pr_008  ~?= 40824
  ]
