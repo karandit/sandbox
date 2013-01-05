2681d6a821186df3105640307f4d6298b3103822
karandit <tkarandi@gmail.com>
2013-01-05 11:32:20 +0100

    Add solution 11 to the infrastructure

--- AllTests.hs
+++ AllTests.hs
@@ -3,9 +3,11 @@ module AllTests (main) where
 import Test.HUnit
 import Pr001
 import Pr008
+import Pr011
 
 main = runTestTT $ test [ 
 	pr_001  ~?= 23331668
 	, pr_008  ~?= 40824
+	, pr_011  ~?= 70600674
  ]
 
