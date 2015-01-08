c5d1d724c7b19cb532a72e2278aeba347cd31c28
karandit <tkarandi@gmail.com>
2015-01-08 21:20:22 +0100

    Add solution for problem 18.

--- AllTests.hs
+++ AllTests.hs
@@ -4,10 +4,12 @@ import Test.HUnit
 import Pr001
 import Pr008
 import Pr011
+import Pr018
 
 main = runTestTT $ test [ 
 	pr_001  ~?= 23331668
 	, pr_008  ~?= 40824
 	, pr_011  ~?= 70600674
+	, pr_018  ~?= 1074
  ]
 
